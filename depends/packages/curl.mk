e39e0f46ea5d14a7a18cba21feb6777c9a4c243d
jayhines91 <81688708+jayhines91@users.noreply.github.com>
2025-09-12 08:20:19 -0500

    Update curl.mk

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -1,20 +1,30 @@
 package=curl
-$(package)_version=7.62.0
+$(package)_version=7.79.1
 $(package)_download_path=https://curl.haxx.se/download/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=55ccd5b5209f8cc53d4250e2a9fd87e6f67dd323ae8bd7d06b072cfcbb7836cb
+$(package)_sha256_hash=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2
 $(package)_dependencies=openssl zlib
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --with-ca-fallback
-$(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-winssl --with-schannel
-$(package)_config_opts_darwin=--enable-sspi --without-ssl --with-darwinssl --with-secure-transport
+$(package)_config_opts+=--disable-cookies
+$(package)_config_opts+=--disable-manual
+$(package)_config_opts+=--disable-unix-sockets
+$(package)_config_opts+=--disable-verbose
+$(package)_config_opts+=--disable-versioned-symbols
+$(package)_config_opts+=--enable-symbol-hiding
+$(package)_config_opts+=--without-librtmp
+$(package)_config_opts+=--disable-rtsp
+$(package)_config_opts+=--disable-alt-svc
+$(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-nss --with-schannel
+$(package)_config_opts_darwin=--enable-sspi --without-ssl --with-secure-transport
+$(package)_config_opts_linux=--with-ssl
 $(package)_cflags=-DCURL_STATICLIB
 $(package)_cxxflags=-std=c++11
 endef
 
 define $(package)_config_cmds
-  ./buildconf && \
+  autoreconf -fi && \
   $($(package)_autoconf)
 endef
 
