a4c5008572d659192634319d43d62b66dded17f6
jayhines91 <81688708+jayhines91@users.noreply.github.com>
2025-09-14 20:10:22 -0500

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -3,15 +3,18 @@ $(package)_version=1_70_0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.70.0/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
 $(package)_sha256_hash=430ae8354789de4fd19ee52f3b1f739e1fba576f0aded0897c3c2bc00fb38778
-$(package)_patches=unused_var_in_process.patch
-$(package)_patches=disable-predef-tools-check.patch
-$(package)_patches=fix_pthread_stack_min.patch
+
+# IMPORTANT: define all patches on ONE line (or use +=). Using '=' multiple times overwrote previous ones.
+$(package)_patches=unused_var_in_process.patch disable-predef-tools-check.patch fix_pthread_stack_min.patch
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
 $(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
+# keep b2 from touching Boost.Test anywhere, belt & suspenders
+$(package)_config_opts+=--without-test
+
 $(package)_config_opts_linux=target-os=linux threadapi=pthread runtime-link=shared
 $(package)_config_opts_darwin=target-os=darwin runtime-link=shared
 $(package)_config_opts_mingw32=target-os=windows binary-format=pe threadapi=win32 runtime-link=static
@@ -22,22 +25,33 @@ $(package)_config_opts_i686_android=address-model=32
 $(package)_config_opts_aarch64_android=address-model=64
 $(package)_config_opts_x86_64_android=address-model=64
 $(package)_config_opts_armv7a_android=address-model=32
+
 $(package)_toolset_$(host_os)=gcc
 $(package)_toolset_darwin=clang
 ifneq (,$(findstring clang,$($(package)_cxx)))
    $(package)_toolset_$(host_os)=clang
 endif
 $(package)_archiver_$(host_os)=$($(package)_ar)
-$(package)_config_libraries=filesystem,system,thread,test
+
+# Only what we need; drop 'test' so it never builds
+$(package)_config_libraries=filesystem,system,thread
+
 $(package)_cxxflags=-std=c++11 -fvisibility=hidden
+# PTHREAD_STACK_MIN & GNU extensions needed by old Boost.Thread on glibc
+$(package)_cxxflags+=-D_GNU_SOURCE -DPTHREAD_STACK_MIN=16384
 $(package)_cxxflags_linux=-fPIC
 $(package)_cxxflags_android=-fPIC
 endef
 
+# Actually apply all patches, then write user-config.jam
 define $(package)_preprocess_cmds
+  set -e; \
+  for p in $($(package)_patches); do \
+    echo "Applying $$p"; \
+    patch -p1 -N -r- -i "$($(package)_patch_dir)/$$p"; \
+  done; \
   echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
-
 
 define $(package)_config_cmds
   ./bootstrap.sh --without-icu --with-libraries=$($(package)_config_libraries) --with-toolset=$($(package)_toolset_$(host_os))
