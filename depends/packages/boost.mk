04bbe5cdc2b6fea92ca40c02f3ce60b31e6d4f94
jayhines91 <81688708+jayhines91@users.noreply.github.com>
2025-09-14 20:35:18 -0500

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -45,11 +45,10 @@ endef
 
 define $(package)_preprocess_cmds
   patch --batch -N -p1 < $($(package)_patch_dir)/unused_var_in_process.patch || true && \
-  patch --batch -N -p1 < $($(package)_patch_dir)/disable-predef-tools-check.patch || true && \
-  patch --batch -N -p1 < $($(package)_patch_dir)/fix_pthread_stack_min.patch || true && \
-  echo 'int main(){return 0;}' > libs/predef/tools/check/predef_check_cc_as_cpp.cpp && \
-  echo 'int main(){return 0;}' > libs/predef/tools/check/predef_check_cc.cpp && \
-  echo 'int main(){return 0;}' > libs/predef/tools/check.cpp && \
+  sed -i -E 's/#if[[:space:]]+PTHREAD_STACK_MIN[[:space:]]*>[[:space:]]*0/#if defined(PTHREAD_STACK_MIN) \&\& (PTHREAD_STACK_MIN+0) > 0/' boost/thread/pthread/thread_data.hpp && \
+  printf 'int main(){return 0;}\n' > libs/predef/tools/check/predef_check_cc_as_cpp.cpp && \
+  printf 'int main(){return 0;}\n' > libs/predef/tools/check/predef_check_cc.cpp && \
+  printf 'int main(){return 0;}\n' > libs/predef/tools/check.cpp && \
   echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
 
