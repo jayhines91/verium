f8fd8b48c1a83337b8d441392bf3898134b41c54
jayhines91 <81688708+jayhines91@users.noreply.github.com>
2025-09-22 23:34:24 -0500

    Update bdb.mk

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -6,11 +6,21 @@ $(package)_sha256_hash=12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b857327
 $(package)_build_subdir=build_unix
 
 define $(package)_set_vars
+# original config knobs
 $(package)_config_opts=--disable-shared --enable-cxx --disable-replication
 $(package)_config_opts_mingw32=--enable-mingw
 $(package)_config_opts_linux=--with-pic
+$(package)_config_opts_darwin=--with-pic
+
 $(package)_cxxflags=-std=c++11
 $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
+
+
+$(package)_cflags=
+$(package)_cflags_darwin+=-O2 -fPIC -mmacosx-version-min=$(MACOSX_DEPLOYMENT_TARGET) -isysroot $(SDKROOT) \
+  -Wno-implicit-int -Wno-implicit-function-declaration -Wno-deprecated-non-prototype
+$(package)_cppflags_darwin+=-D_DARWIN_C_SOURCE
+$(package)_ldflags_darwin+=-mmacosx-version-min=$(MACOSX_DEPLOYMENT_TARGET) -isysroot $(SDKROOT)
 endef
 
 define $(package)_preprocess_cmds
@@ -20,6 +30,10 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
+  CC="$(CC)" CXX="$(CXX)" \
+  CFLAGS="$(CFLAGS) $($(package)_cflags) $($(package)_cflags_darwin)" \
+  CPPFLAGS="$(CPPFLAGS) $($(package)_cppflags_darwin)" \
+  LDFLAGS="$(LDFLAGS) $($(package)_ldflags_darwin)" \
   ../dist/$($(package)_autoconf)
 endef
 
