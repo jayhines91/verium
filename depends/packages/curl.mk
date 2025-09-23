52036c1392896f1cf220d4186dd5afe9ab56c1dc
jayhines91 <81688708+jayhines91@users.noreply.github.com>
2025-09-22 21:08:52 -0500

    Update curl.mk

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -6,6 +6,7 @@ $(package)_sha256_hash=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0e
 $(package)_dependencies=openssl zlib
 
 define $(package)_set_vars
+# build a static libcurl with minimal features
 $(package)_config_opts=--disable-shared --with-ca-fallback
 $(package)_config_opts+=--disable-cookies
 $(package)_config_opts+=--disable-manual
@@ -16,15 +17,24 @@ $(package)_config_opts+=--enable-symbol-hiding
 $(package)_config_opts+=--without-librtmp
 $(package)_config_opts+=--disable-rtsp
 $(package)_config_opts+=--disable-alt-svc
-$(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-nss --with-schannel
-$(package)_config_opts_darwin=--enable-sspi --without-ssl --with-secure-transport
-$(package)_config_opts_linux=--with-ssl
+
+# Use OpenSSL from depends everywhere (more reproducible than Secure Transport)
+$(package)_config_opts_linux=--with-openssl="$(host_prefix)"
+$(package)_config_opts_darwin=--with-openssl="$(host_prefix)"
+# If you *really* want Secure Transport on macOS, swap the line above for:
+# $(package)_config_opts_darwin=--with-secure-transport --without-openssl
+
+# (Optional: if you ever target mingw)
+$(package)_config_opts_mingw32=--with-openssl="$(host_prefix)"
+
 $(package)_cflags=-DCURL_STATICLIB
 $(package)_cxxflags=-std=c++11
+# (Optional: make min macOS explicit)
+$(package)_cflags_darwin=-mmacosx-version-min=$(MACOSX_DEPLOYMENT_TARGET)
 endef
 
+# DO NOT autoreconf on macOS: use the tarball's shipped configure
 define $(package)_config_cmds
-  autoreconf -fi && \
   $($(package)_autoconf)
 endef
 
